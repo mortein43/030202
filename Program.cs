0ebd8569abbfa817b148f34472705da6c2b417d9
mortein43 <mortein43@gmail.comm>
2023-10-31 09:59:48 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace _030202
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://www.philpapers.org/");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader stream = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+            stream.Close();
+            Console.WriteLine("Заголовки: ");
+            foreach(string header in response.Headers)
+            {
+                Console.WriteLine("{0}:{1}", header, response.Headers[header]);
+            }
+
+
+        }
+    }
+}
